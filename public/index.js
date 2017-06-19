c560cb1e4903f48ad9b6ef98d683648cfc191755
kmng1 <kmng@alumni.nus.edu.sg>
2017-06-19 17:35:07 +0800

    complete first version

--- public/index.js
+++ public/index.js
@@ -6,6 +6,23 @@
     //Create a function constructor.
     var MyCon = function(){
         var myCon = this;
+        myCon.submitForm = function(){
+            for(var i in myCon){
+                if (typeof(myCon[i])!="function") console.log("Input: %s = %s", i, myCon[i]);
+            }
+            console.log("Feedback submited");
+            myCon.resetForm();
+        };
+
+        myCon.resetForm = function(){
+            myCon.name = "";
+            myCon.email = "";
+            myCon.phone = "";
+            myCon.comment = "";
+            myCon.exp = "";
+        };
+       myCon.resetForm();
+
     };
     //Use the function as a controller
 	MainApp.controller("MyCon", MyCon);
